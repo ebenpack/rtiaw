6e7857d07f9cff7e92aa0c0d4eae19016c1b1c03
ebenpack <ebenpackwood@gmail.com>
2021-04-11 18:23:56 -0400

    add material to hit_record

--- src/object/object.rs
+++ src/object/object.rs
@@ -1,11 +1,14 @@
+use crate::material::Material;
 use crate::ray::Ray;
 use crate::vec3::Vec3;
+use std::sync::Arc;
 
 pub struct HitRecord {
     pub p: Vec3,
     pub normal: Vec3,
     pub t: f64,
     pub front_face: bool,
+    pub material: Arc<dyn Material>,
 }
 
 impl HitRecord {
