28d822812858cf9c0327d96918914a469dcd9734
ebenpack <ebenpackwood@gmail.com>
2021-04-11 11:33:20 -0400

    add front_face

--- src/object/object.rs
+++ src/object/object.rs
@@ -5,6 +5,18 @@ pub struct HitRecord {
     pub p: Vec3,
     pub normal: Vec3,
     pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, ray: &Ray, outward_normal: Vec3) {
+        self.front_face = Vec3::dot(&ray.direction, &outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            outward_normal
+        } else {
+            -outward_normal
+        };
+    }
 }
 
 pub trait Object {
