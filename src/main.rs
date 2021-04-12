88b8cb27caaf54c93917c5f8e208284cfed13e4f
ebenpack <ebenpackwood@gmail.com>
2021-04-12 07:38:08 -0400

    add random scene generator

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use crate::camera::Camera;
 use crate::color::Color;
 use crate::image::{Image, PPM};
-use crate::material::{Lambertian, Metal};
+use crate::material::{Dielectric, Lambertian, Metal};
 use crate::object::{HitRecord, Object, Sphere};
 use crate::ray::Ray;
 use crate::scene::Scene;
@@ -66,47 +66,96 @@ fn ray_color(ray: &Ray, scene: &Scene, depth: i32) -> Color {
     return (1.0 - t) * &Color::new(1.0, 1.0, 1.0) + t * &Color::new(0.5, 0.7, 1.0);
 }
 
-fn main() -> std::io::Result<()> {
-    // Image
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width = 1200;
-    let image_height = (image_width as f64 / aspect_ratio) as i32;
-    let samples_per_pixel = 100;
-    let max_depth = 50;
-
-    // Camera
-    let camera = Arc::new(Camera::new());
-
-    // Scene
+fn random_scene() -> Scene {
     let mut scene = Scene::new();
 
-    let material_ground = Arc::new(Lambertian::new(Color::new(0.6627, 0.9882, 0.8196)));
-    let material_center = Arc::new(Lambertian::new(Color::new(0.8862, 0.08627, 0.1647)));
-    let material_left = Arc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.0));
-    let material_right = Arc::new(Metal::new(Color::new(0.968, 0.6627, 0.8235), 1.0));
-
+    let ground_material = Arc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
     scene.add(Arc::new(Sphere::new(
-        Vec3::new(0.0, -100.5, -1.0),
-        100.0,
-        material_ground,
-    )));
-    scene.add(Arc::new(Sphere::new(
-        Vec3::new(0.0, 0.0, -1.0),
-        0.5,
-        material_center,
-    )));
-    scene.add(Arc::new(Sphere::new(
-        Vec3::new(-1.0, 0.0, -1.0),
-        0.5,
-        material_left,
-    )));
-    scene.add(Arc::new(Sphere::new(
-        Vec3::new(1.0, 0.0, -1.0),
-        0.5,
-        material_right,
+        Vec3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
     )));
 
-    let scene = Arc::new(scene);
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rand::thread_rng().gen_range(0.0..1.0);
+            let rand_num1 = rand::thread_rng().gen_range(0.0..1.0);
+            let rand_num2 = rand::thread_rng().gen_range(0.0..1.0);
+
+            let center = Vec3::new(a as f64 + 0.9 * rand_num1, 0.2, b as f64 + 0.9 * rand_num2);
+
+            if (center - Vec3::new(4.0, 0.2, 0.0)).len() > 0.9 {
+                if choose_mat < 0.8 {
+                    // diffuse
+                    let albedo = Color::random(0.0, 1.0) * Color::random(0.0, 1.0);
+                    let sphere_material = Arc::new(Lambertian::new(albedo));
+                    scene.add(Arc::new(Sphere::new(center, 0.2, sphere_material)));
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = Color::random(0.5, 1.0);
+                    let fuzz = rand::thread_rng().gen_range(0.0..0.5);
+                    let sphere_material = Arc::new(Metal::new(albedo, fuzz));
+                    scene.add(Arc::new(Sphere::new(center, 0.2, sphere_material)));
+                } else {
+                    // glass
+                    let sphere_material = Arc::new(Dielectric::new(1.5));
+                    scene.add(Arc::new(Sphere::new(center, 0.2, sphere_material)));
+                }
+            }
+        }
+    }
+
+    let material1 = Arc::new(Dielectric::new(1.5));
+    scene.add(Arc::new(Sphere::new(
+        Vec3::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
+    )));
+
+    let material2 = Arc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    scene.add(Arc::new(Sphere::new(
+        Vec3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
+    )));
+
+    let material3 = Arc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+    scene.add(Arc::new(Sphere::new(
+        Vec3::new(4.0, 1.0, 0.0),
+        1.0,
+        material3,
+    )));
+
+    scene
+}
+
+fn main() -> std::io::Result<()> {
+    // Image
+    let aspect_ratio = 3.0 / 2.0;
+    let image_width = 1200;
+    let image_height = (image_width as f64 / aspect_ratio) as i32;
+    let samples_per_pixel = 500;
+    let max_depth = 200;
+
+    // Camera
+    let look_from = Vec3::new(13.0, 2.0, 3.0);
+    let look_at = Vec3::new(0.0, 0.0, 0.0);
+    let vup = Vec3::new(0.0, 1.0, 0.0);
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
+
+    let camera = Arc::new(Camera::new(
+        &look_from,
+        &look_at,
+        &vup,
+        20.0,
+        aspect_ratio,
+        aperture,
+        dist_to_focus,
+    ));
+
+    // Scene
+    let scene = Arc::new(random_scene());
 
     struct Job {
         x: i32,
